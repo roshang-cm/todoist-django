0a58f863bc0c05739f8930d3753045f90a1f2813
Roshan G <roshan.g@codingmart.com>
2020-03-24 12:53:01 +0530

    Added

--- todoist/settingsprod.py
+++ todoist/settingsprod.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -126,3 +127,5 @@ CORS_ORIGIN_ALLOW_ALL = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
