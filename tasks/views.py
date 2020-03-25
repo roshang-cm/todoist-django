3531539a2844b3634632fb0f7d905c8953863674
Roshan G <roshan.g@codingmart.com>
2020-03-26 00:42:13 +0530

    Fixed

--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,7 @@ from tasks.models import Label, Project, Section, Task
 from users.models import User
 from tasks.serializers import TaskCreateSerializer, TaskSerializer
 from todoist.response import resolve_user_id_from_jwt, StandardResponse
+from json import loads, dumps
 # Create your views here.
 
 
@@ -21,13 +22,16 @@ class TaskView(APIView):
     def post(self, request):
         try:
             user_id = resolve_user_id_from_jwt(request)
+            request_data = request.POST
+            if request.body:
+                request_data = loads(request.body.decode('utf-8'))
             task_serializer = TaskCreateSerializer(
-                data={**request.data.dict(), 'user': user_id})
+                data={**request_data.dict(), 'user': user_id})
             if not task_serializer.is_valid():
                 print(task_serializer.initial_data)
                 return StandardResponse(errors=task_serializer.errors)
             else:
-                task_id = request.data.get('uuid')
+                task_id = request_data.get('uuid')
                 if task_id:
                     try:
                         task = Task.objects.get(uuid=task_id)
@@ -36,7 +40,7 @@ class TaskView(APIView):
                         section = task.section
                         parent = task.parent
                         for field in ['project', 'label', 'section', 'parent']:
-                            form_data = request.DATA.get(field)
+                            form_data = request_data.get(field)
                             if form_data:
                                 if field == 'project':
                                     project = Project.objects.get(pk=form_data)
@@ -46,18 +50,18 @@ class TaskView(APIView):
                                     section = Section.objects.get(pk=form_data)
                                 if field == 'parent':
                                     parent = Task.objects.get(pk=form_data)
-                        task.title = request.DATA.get('title', task.title)
+                        task.title = request_data.get('title', task.title)
                         task.project = project
                         task.label = label
-                        task.due_date = request.DATA.get(
+                        task.due_date = request_data.get(
                             'due_date', task.due_date)
-                        task.priority = request.DATA.get(
+                        task.priority = request_data.get(
                             'priority', task.priority)
                         task.section = section
                         task.parent = parent
-                        task.checked = request.DATA.get(
+                        task.checked = request_data.get(
                             'checked', task.checked)
-                        task.order = request.DATA.get('order', task.order)
+                        task.order = request_data.get('order', task.order)
                         task.save()
                     except Exception as e:
                         return StandardResponse(errors=str(e))
--- users/views.py
+++ users/views.py
@@ -2,10 +2,14 @@ from django.contrib.auth.hashers import check_password
 from todoist.response import StandardResponse, get_jwt_for_id
 from users.serializers import UserCreateSerializer, UserSerializer
 from users.models import User
+from json import loads, dumps
 
 
 def register(request):
-    user_data = UserCreateSerializer(data=request.DATA)
+    request_data = request.POST
+    if request.body:
+        request_data = loads(request.body.decode('utf-8'))
+    user_data = UserCreateSerializer(data=request_data)
     data = None
     if user_data.is_valid():
         user = user_data.save()
@@ -19,8 +23,11 @@ def register(request):
 
 
 def login(request):
-    username = request.DATA.get('username')
-    password = request.DATA.get('password')
+    request_data = request.POST
+    if request.body:
+        request_data = loads(request.body.decode('utf-8'))
+    username = request_data.get('username')
+    password = request_data.get('password')
     if not username:
         return StandardResponse(errors='username missing')
     try:
